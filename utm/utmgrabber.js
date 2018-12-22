c26bc972e1b6c357d428e239753c33efc184367a
Hashim Warren <hashimwarren@gmail.com>
2018-12-21 20:34:57 -0500

    Add serverless function

--- utm/utmgrabber.js
+++ utm/utmgrabber.js
@@ -1,6 +1,8 @@
 // reference - https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
 // create empty object. Then fill object with key/value pairs of the URL's parameters
 
+let ourForm = document.getElementById("ourForm")
+
 function arrayURLtoObject() {
     let params = (new URL(document.location)).searchParams
     let e = {}
@@ -32,15 +34,33 @@ localData = JSON.parse(localStorage.getItem('urlParameters'))
 console.log(localData)
 
 //initiate FormData object
-const submittedData = new FormData()
+const submittedData = new FormData(ourForm)
 
 //Object.entries() method returns an array,
 //forEach iterates over it,
 //FormData.append inserts the key, value pairs
-Object.entries(localData).forEach(([key, value]) =>  submittedData.append(key, value))
 
-console.log(submittedData.getAll("utm_source"))
-console.log(submittedData.getAll("utm_medium"))
+
+
+
+
+request = new XMLHttpRequest()
+
+ourForm.addEventListener("submit", (e) => {
+    e.preventDefault() // prevent form from reloading page
+    submitFormToMailchimp()
+
+  })
+
+function submitFormToMailchimp() {
+    request.open("POST", "https://wt-a7517fede6766ea5c54bb2d101927597-0.sandbox.auth0-extend.com/add-to-mailchimp")
+    Object.entries(localData).forEach(([key, value]) =>  submittedData.append(key, value))
+    request.send(submittedData)
+    console.log(submittedData.getAll("utm_source"))
+    console.log(submittedData.getAll("utm_medium"))
+
+}
+
 
 
 
