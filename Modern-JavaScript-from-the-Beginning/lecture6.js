0397e151f610145936d09a35f732a686eaefa18c
Hashim Warren <hashimwarren@gmail.com>
2019-01-01 23:02:45 -0500

    Re-learned var. let, const

---
+++ Modern-JavaScript-from-the-Beginning/lecture6.js
@@ -0,0 +1,43 @@
+var name = 'John Doe'
+console.log(name)
+
+name = 'Hashim Warren'
+console.log(name)
+
+//initialize variable
+
+var greeting
+
+console.log(greeting)
+
+// variables can only have letters, numbers, _underscore_, and $ sign
+//cannot start with a number
+
+// underscores conventionally used for private variables
+// $variable convertionally used with jQuery
+
+//classes and constructor functions convertionally use uppercase word
+
+let ClassOrConstructor
+
+// const cannot be reassigned or initialized without value
+
+const father = "Hashim"
+console.log(father)
+
+//father = "george"
+//console.log(father) //type error
+
+// const allows reassignment of properties in object
+
+const family = {
+    dad: "Hashim",
+    mom: "Alex"
+
+}
+
+family.dad = "george"
+console.log(family.dad)
+
+//default to const unless I'm initializing the variable or using it in iterator
+
