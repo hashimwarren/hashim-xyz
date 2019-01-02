a3e8a93ab332b3b5664b4dfdd65b04803c3b5647
Hashim Warren <hashimwarren@gmail.com>
2019-01-02 13:03:10 -0500

    Add Math object lesson

---
+++ Modern-JavaScript-from-the-Beginning/lecture9.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+//Simple math with number
+
+val = num1 + num2;
+val = num1 * num2;
+val = num1 % num2; // modulus operator gets remainder
+
+console.log(val);
+
+//Math Object
+
+val = Math.PI
+val = Math.round(2.4)
+val = Math.ceil(2.1)
+val = Math.min(2,1,3,0)
+val = Math.max(2,1,3,0)
+val = Math.floor(Math.random() * 20 +1)
+
+console.log(val);
+console.log(val);
+console.log(val);
+console.log(val);
+console.log(val);
+console.log(val);
+
