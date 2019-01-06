ee25886d6061b22921819b8a894808c7aa9fdf16
Hashim Warren <hashimwarren@gmail.com>
2019-01-05 23:02:17 -0500

    Practice strings

---
+++ Modern-JavaScript-from-the-Beginning/lecture10.js
@@ -0,0 +1,35 @@
+const firstName = 'William'
+const lastName = 'Johnson'
+
+let val;
+
+val = firstName + lastName
+
+//Concatenation
+val = firstName + ' ' + lastName
+
+
+// append
+val = 'Hashim '
+val += 'Warren'
+
+//escaping
+
+val = 'That\'s awesome, I can\'t wait'
+
+//length
+
+val = firstName.length
+
+//concatenation part 2
+val = firstName.concat(' ', lastName)
+
+//change case
+val = firstName.toLowerCase()
+val = firstName.toUpperCase()
+
+
+
+
+console.log(val)
+
