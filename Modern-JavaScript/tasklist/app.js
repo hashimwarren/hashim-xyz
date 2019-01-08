61b127c99e98df2e622becab932824e30200949f
Hashim Warren <hashimwarren@gmail.com>
2019-01-07 22:39:55 -0500

    define UI variable

---
+++ Modern-JavaScript/tasklist/app.js
@@ -0,0 +1,8 @@
+//Define UI variables
+
+const form =        document.querySelector('#task-form')
+const taskList =    document.querySelector('.collection')
+const clearBtn =    document.querySelector('.clear-tasks')
+const filter =      document.querySelector('#filter')
+const taskInput =   document.querySelector('#task')
+
