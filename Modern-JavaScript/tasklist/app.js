2370a3d49c0943f63a17c14ed4839622d33975bf
Hashim Warren <hashimwarren@gmail.com>
2019-01-12 18:07:33 -0500

    Add ability to add tasks

--- Modern-JavaScript/tasklist/app.js
+++ Modern-JavaScript/tasklist/app.js
@@ -6,3 +6,41 @@ const clearBtn =    document.querySelector('.clear-tasks')
 const filter =      document.querySelector('#filter')
 const taskInput =   document.querySelector('#task')
 
+//load all event listeners
+
+loadEventListeners()
+
+// load all event listeners
+
+function loadEventListeners() {
+    //add task event
+    form.addEventListener('submit', addTask)
+}
+
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a Task')
+    }
+
+    const li = document.createElement('li')
+    //add class
+    li.className = 'collectionItem'
+    //create text node and append li
+    li.appendChild(document.createTextNode(taskInput.value))
+    //create new link element
+    const link = document.createElement('a')
+    link.className = 'delete-item secondary'
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    //apend the link to the li
+    li.appendChild(link)
+
+    //append li to the ul
+    taskList.appendChild(li)
+
+    //clear input
+    taskInput.value = ''
+
+
+    e.preventDefault()
+
+}
