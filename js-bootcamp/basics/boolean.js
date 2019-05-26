971b28626f7e343b4e1ec8e76384846c65ccfcf3
Hashim Warren <hashimwarren@gmail.com>
2019-05-26 15:27:24 -0400

    lesson 14 - booleans and comparison operators

---
+++ js-bootcamp/basics/boolean.js
@@ -0,0 +1,25 @@
+// === is the equality operator
+// !== no equal
+// < less than
+// > greater than
+// <= and >= less than or equal, greater than or equal
+
+let temp = 31
+let isFreezing = temp <= 32
+
+console.log(isFreezing)
+
+// CHALLENGE
+
+//Create age set to your age
+// calculatw is child - if they are 7 or under
+// calculate if senior is 65 or older
+//print is child value
+//print is senoir value
+
+let age = 300
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
